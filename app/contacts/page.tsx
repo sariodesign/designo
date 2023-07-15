d38d7a44ae6d1c267a7f99556354ea15b8d791d9
Rosario <sariodesign.it@gmail.com>
2023-07-15 12:01:00 +0200

    Init contact page

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,7 +1,47 @@
+import { InfoContent } from '../ui/InfoContent'
+import { BtnVariant } from '../ui/Btn'
+import { Footer } from "../ui/Footer"
+
+const infoContentList = [
+  {
+    imageSrc: "/illustration-canada.svg",
+    bgGradient: "bg-gradient-to-r from-gradient-info",
+    name: "Canada",
+    btn: {
+      label: "See location",
+      href: "/",
+      variant: BtnVariant.Primary
+    }
+  },
+  {
+    
+    imageSrc: "/illustration-australia.svg",
+    bgGradient: "bg-gradient-to-t from-gradient-info",
+    name: "Australia",
+    btn: {
+      label: "See location",
+      href: "/",
+      variant: BtnVariant.Primary
+    }
+  },
+  {
+    
+    imageSrc: "/illustration-united-kingdom.svg",
+    bgGradient: "bg-gradient-to-b from-gradient-info",
+    name: "United Kingdom",
+    btn: {
+      label: "See location",
+      href: "/",
+      variant: BtnVariant.Primary
+    }
+  }
+]
+
 export default function Contacts() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Contacts
+    <main className="min-h-screen">
+      <InfoContent contents={infoContentList} />
+      <Footer />
     </main>
   )
 }
