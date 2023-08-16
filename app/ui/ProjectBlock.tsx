36209f914b932c4a2fd934d72a641872492896f7
Rosario <sariodesign.it@gmail.com>
2023-08-16 16:49:32 +0200

    Updated project code

--- app/ui/ProjectBlock.tsx
+++ app/ui/ProjectBlock.tsx
@@ -11,11 +11,11 @@ export function Project({...props}:ProjectProps) {
 	let mbClass = props.lastItem ? `mb-24 md:mb-0` : `mb-10 sm:mb-8 md:mb-0`
 
 	return (
-		<div className={`rounded-[15px] flex flex-col overflow-hidden sm:flex-row md:flex-col ${mbClass}`}>
+		<div className={`group rounded-[15px] flex flex-col overflow-hidden sm:flex-row md:flex-col cursor-pointer ${mbClass}`}>
 			<img className="sm:w-1/2 md:w-full" src={props.preview} alt={props.name} />
-			<div className="bg-very-light-peach flex flex-col items-center justify-center p-8 text-[26px] sm:w-1/2 md:w-full">
-				<h3 className="mb-4 text-peach uppercase">{props.name}</h3>
-				<p className="text-center">{props.description}</p>
+			<div className="bg-very-light-peach flex flex-col items-center justify-center p-8 sm:w-1/2 md:group-hover:bg-peach md:w-full">
+				<h3 className="mb-4 text-peach uppercase text-[20px] group-hover:text-white">{props.name}</h3>
+				<p className="text-center text-dark-grey group-hover:text-white">{props.description}</p>
 			</div>
 		</div>
 	)
--- app/ui/ProjectList.tsx
+++ app/ui/ProjectList.tsx
@@ -4,7 +4,7 @@ export function ProjectList({projects}:{projects: ProjectProps[]}) {
 
 
 	return (
-		<div className="px-6 md:grid md:grid-cols-3 md:grid-rows-2 md:gap-8">
+		<div className="px-6 md:grid md:grid-cols-3 md:grid-rows-2 md:gap-8 md:px-0 md:mb-40">
 			{projects.map(item => <Project key={item.name}  {...item}/>)}
 		</div>
 	)
